382e837a1b7f7f973207c93c1e2e708b2e34f11b
Wassim-Aloui <61364693+Wassim-Aloui@users.noreply.github.com>
2023-01-05 16:47:59 +0100

    tunikoora symfony project

---
+++ config/bundles.php
@@ -0,0 +1,51 @@
+<?php
+
+
+return array(
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => array('all' => true),
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => array('all' => true),
+    Symfony\Bundle\TwigBundle\TwigBundle::class => array('all' => true),
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => array('dev' => true, 'test' => true),
+    Symfony\Bundle\MonologBundle\MonologBundle::class => array('all' => true),
+    Symfony\Bundle\DebugBundle\DebugBundle::class => array('dev' => true),
+    Symfony\Bundle\MakerBundle\MakerBundle::class => array('dev' => true),
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => array('all' => true),
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => array('all' => true),
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => array('all' => true),
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => array('all' => true),
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => array('all' => true),
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => array('all' => true),
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => array('all' => true),
+    MercurySeries\FlashyBundle\MercurySeriesFlashyBundle::class => array('all' => true),
+    MercurySeries\FlashyBundle\MercurySeriesFlashyBundle::class => array('all' => true),
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => array('dev' => true),
+    CalendarBundle\CalendarBundle::class => array('all' => true),
+
+);
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    MercurySeries\FlashyBundle\MercurySeriesFlashyBundle::class => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+
+    Vangrg\ProfanityBundle\VangrgProfanityBundle::class => ['all' => true],
+
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+
+
+
+];
+
+
